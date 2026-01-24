943f91d531e0f077e04631572cb2369607583216
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2026-01-24 13:14:40 -0300

    feat: Implement CRUD operations for invoices with validation and error handling

---
+++ nextjs-dashboard/app/lib/actions.ts
@@ -0,0 +1,79 @@
+"use server";
+
+import { z } from "zod";
+import { date } from "zod/v4";
+import postgres from "postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+const formSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const createInvoiceValidationForm = formSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  try {
+    const { customerId, amount, status } = createInvoiceValidationForm.parse({
+      customerId: formData.get("customerId"),
+      amount: formData.get("amount"),
+      status: formData.get("status"),
+    });
+
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split("T")[0];
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  } catch (error) {
+    console.log(error);
+    return {
+      message: "Database Error: Failed to Create Invoice.",
+    };
+  }
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+// Use Zod to update the expected types
+const updateInvoceFormValidation = formSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  try {
+    const { customerId, amount, status } = updateInvoceFormValidation.parse({
+      customerId: formData.get("customerId"),
+      amount: formData.get("amount"),
+      status: formData.get("status"),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+  } catch (error) {
+    console.log(error);
+    return {
+      message: "Database Error: Failed to Update Invoice.",
+    };
+  }
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
+}
